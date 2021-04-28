cb04b18f0f60713561d361c76189a7adf7aee949
jbowie48 <johnise_bowie76@student.stmaryk12.net>
2021-04-28 17:55:29 +0000

    adding circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,12 +20,24 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
+        var circle; 
+        var circles = [];
 
 
         // TODO 2 : Create a function that draws a circle 
+        function drawCircle(){
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
         
 
         // TODO 3 / 7 : Call the drawCircle() function 
+
+        for (var count = 0; count < 100; count++) {
+            drawCircle();
+        }
 
 
         ////////////////////////////////////////////////////////////
@@ -39,15 +51,19 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
+            
 
             
             // TODO 5 : Call game.checkCirclePosition() on your circles.
            
 
             // TODO 8 : Iterate over the array
-           
-            
+           for (var i = 0; i < circles.length; i++) {
+                var eachCircle = circles[i];
+                 physikz.updatePosition(eachCircle);
+                  game.checkCirclePosition(eachCircle);
         }
+    };
     
         /* 
         This Function should check the position of a circle that is passed to the 
@@ -62,6 +78,15 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
+            if (circle.x < 0) {
+                circle.x = canvas.width
+            }
+            if (circle.y < 0) {
+                circle.y = canvas.height
+            }
+            if (circle.y > canvas.height) {
+                circle.x = 0;
+            }
             
 
 
